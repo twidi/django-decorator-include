2fc59fe6c3c8bd57555a2f042d97bd615ca386dc
Stephane Angel (Twidi) <s.angel@twidi.com>
2015-12-15 22:15:40 +0100

    Restore the urlconf_module property
    This reverts 9d30dad993363f18c711d7140131b34f92efbd9d because it's not a good idea
    to import modules when we instanciate a DecoratedPatterns instance

--- src/decorator_include/__init__.py
+++ src/decorator_include/__init__.py
@@ -21,15 +21,16 @@ class DecoratedPatterns(object):
     A wrapper for an urlconf that applies a decorator to all its views.
     """
     def __init__(self, urlconf_name, decorators):
+        self.urlconf_name = urlconf_name
         try:
             iter(decorators)
         except TypeError:
             decorators = [decorators]
         self.decorators = decorators
-        if isinstance(urlconf_name, str):
-            self.urlconf_module = import_module(urlconf_name)
+        if not isinstance(urlconf_name, str):
+            self._urlconf_module = self.urlconf_name
         else:
-            self.urlconf_module = urlconf_name
+            self._urlconf_module = None
 
     def decorate_pattern(self, pattern):
         if isinstance(pattern, RegexURLResolver):
@@ -62,6 +63,12 @@ class DecoratedPatterns(object):
             patterns = self.urlconf_module
         return [self.decorate_pattern(pattern) for pattern in patterns]
     urlpatterns = property(_get_urlpatterns)
+
+    def _get_urlconf_module(self):
+        if self._urlconf_module is None:
+            self._urlconf_module = import_module(self.urlconf_name)
+        return self._urlconf_module
+    urlconf_module = property(_get_urlconf_module)
 
     def __getattr__(self, name):
         return getattr(self.urlconf_module, name)
