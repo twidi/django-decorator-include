02d298848f0a03e1fac531d7ea55b884d1fd9825
Jon Dufresne <jon.dufresne@gmail.com>
2016-08-29 20:53:53 -0700

    Add support for Django 1.10

--- src/decorator_include/tests/included.py
+++ src/decorator_include/tests/included.py
@@ -1,12 +1,12 @@
 from __future__ import unicode_literals
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.http import HttpResponse
 
 
 def testify(request):
     return HttpResponse('testify!')
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^included/', include('decorator_include.tests.included2')),
     url(r'^test/$', testify, name='testify'),
-)
+]
