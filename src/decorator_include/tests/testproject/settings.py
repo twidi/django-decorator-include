555845521d669c20595ca01ec72fff70ab37df1c
Jon Dufresne <jon.dufresne@gmail.com>
2017-01-12 19:16:55 -0800

    Use in memory SQLite database for tests

--- src/decorator_include/tests/testproject/settings.py
+++ src/decorator_include/tests/testproject/settings.py
@@ -1,20 +1,13 @@
 from __future__ import unicode_literals
-import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 SECRET_KEY = '8qw5o&7!g&4kg#+4jr6y%qoj6(1s1ufjqo1#x)fqaca&)$2)ba'
 
-BASE_DIR = os.path.dirname(__file__)
-
-def absolute_path(path):
-    return os.path.normpath(os.path.join(BASE_DIR, path))
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': absolute_path('database.sqlite3'),
     }
 }
 
