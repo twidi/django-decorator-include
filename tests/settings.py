3bfe75ee22b004d20aa5d9bb7f41f414d1e9bfb0
Jon Dufresne <jon.dufresne@gmail.com>
2018-01-21 08:11:33 -0800

    Remove test workaround for unsupported Django < 1.10

--- tests/settings.py
+++ tests/settings.py
@@ -21,8 +21,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-# Django < 1.10
-MIDDLEWARE_CLASSES = MIDDLEWARE
 
 ROOT_URLCONF = 'tests.urls'
 
